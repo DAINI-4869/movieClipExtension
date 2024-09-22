73f57a76bb5f27365cc95fbc250a13c2515df9aa
sabamiso <yabuki1374@gmail.com>
2024-09-22 21:26:12 +0900

    レビュー該当条件修正、 一部変数をcamelcaseに変更 不要行削除

--- content.js
+++ content.js
@@ -16,40 +16,37 @@ window.addEventListener('load', function() {
   let Startgettime;
   let Endgettime;
   //urlを取得
-  let path = window.location.pathname;// このデータは　netflixの場合 /watch/00000000 のような出力をする。
-  //urlの変更をチェックするキー
-  let checkurl = false; //　変更時、trueになります。使用後にfalseに戻しておいてください
+  let path = window.location.pathname;
 
   recordButton.addEventListener('click', function() {
     const videoPlayer = document.querySelector('video'); 
-    const alltitlename =  document.querySelector('[data-uia="video-title"]');    //titleの全情報を取得
+    const allTitleName =  document.querySelector('[data-uia="video-title"]');    //titleの全情報を取得
     if (recording) {
-      let titlename;
+      let titleName;
   
-      if(alltitlename){
-        const h4Element = alltitlename.querySelector('h4');
+      if(allTitleName){
+        const h4Element = allTitleName.querySelector('h4');
 
         if(h4Element){
           //シリーズ作品の場合
-          titlename = alltitlename.querySelector('h4').textContent;//エピソード名を取得
-          let episodenumber = document.querySelector('div[data-uia="video-title"] span:nth-of-type(1)').textContent; //エピソード番号を取得
+          titleName = allTitleName.querySelector('h4').textContent;//エピソード名を取得
+          let episodeNumber = document.querySelector('div[data-uia="video-title"] span:nth-of-type(1)').textContent; //エピソード番号を取得
           let subtitle = document.querySelector('div[data-uia="video-title"] span:nth-of-type(2)').textContent;    //サブタイトルを取得
-
           Endgettime = videoPlayer.currentTime;
-          sendData({ StartTime: Startgettime, EndTime: Endgettime ,URL: path , title: titlename , epnumber : episodenumber}); 
+          sendData({ StartTime: Startgettime, EndTime: Endgettime ,URL: path , title: titleName , epnumber : episodeNumber}); 
         } else {
           //シリーズ作品ではない場合　
-          titlename = alltitlename.textContent;//エピソード名を取得
-
+          titlename = allTitleName.textContent;//エピソード名を取得
           Endgettime = videoPlayer.currentTime;
-          sendData({ StartTime: Startgettime, EndTime: Endgettime ,URL: path , title: titlename }); 
+          sendData({ StartTime: Startgettime, EndTime: Endgettime ,URL: path , title: titleName }); 
           
         }
       }else{
-        //error時にも一応遅れるだけ送れるようにしてある。
-        console.log('[data-uia="video-title"] 属性を持つ要素が存在しません。');
+        //error時にも一応送れるだけ送れるようにしてある。
+        console.warn('[data-uia="video-title"] 属性を持つ要素が存在しません。');
         Endgettime = videoPlayer.currentTime;
         sendData({ StartTime: Startgettime, EndTime: Endgettime ,URL: path }); 
+
       }
     
       recording = false;
@@ -80,12 +77,8 @@ window.addEventListener('load', function() {
       let nowpath = window.location.pathname;
       if(path != nowpath){
         path = nowpath;
-        checkurl = true;
-        console.log("urlの変更を検出しました",checkurl); 
+        console.log("urlの変更を検出しました"); 
       }
-
-      
-
       if (mutation.type === 'childList') {
         const controlsForward10Element = document.querySelector('[data-uia="control-forward10"]'); // 広告時に表示しないため
         if (controlsForward10Element) {
