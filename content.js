cfb4a6761a15c6306550d68b343c9b764e0b8153
DAINI <apdk_4869@yahoo.co.jp>
2024-10-02 15:41:21 +0900

    SVGとinitの修正

--- content.js
+++ content.js
@@ -68,7 +68,38 @@
       style.textContent = ".cls-637630c1c3a86d32eae6f029-1{fill:none;stroke:currentColor;stroke-miterlimit:10;}";
       svg.appendChild(style);
 
-      // SVG要素の追加（省略）
+      // SVG要素の追加
+      svg.appendChild(createSVGElement("rect", {
+        class: "cls-637630c1c3a86d32eae6f029-1",
+        x: "1.5",
+        y: "9.14",
+        width: "15.27",
+        height: "12.41"
+      }));
+      
+      svg.appendChild(createSVGElement("polygon", {
+        class: "cls-637630c1c3a86d32eae6f029-1",
+        points: "16.77 17.73 21.55 21.55 22.5 21.55 22.5 9.14 21.55 9.14 16.77 12.96 16.77 17.73"
+      }));
+
+      svg.appendChild(createSVGElement("circle", {
+        class: "cls-637630c1c3a86d32eae6f029-1",
+        cx: "4.84",
+        cy: "5.8",
+        r: "3.34"
+      }));
+
+      svg.appendChild(createSVGElement("circle", {
+        class: "cls-637630c1c3a86d32eae6f029-1",
+        cx: "13.43",
+        cy: "5.8",
+        r: "3.34"
+      }));
+
+      svg.appendChild(createSVGElement("polygon", {
+        class: "cls-637630c1c3a86d32eae6f029-1",
+        points: "7.23 16.77 7.23 13.91 10.09 15.34 7.23 16.77"
+      }));
 
       return svg;
     }
@@ -126,8 +157,7 @@
           }
 
           sendData(data);
-          isRecording = false;
-          svgElement.setAttribute("color", COLOR_DEFAULT);
+          init();
         } else {
           svgElement.setAttribute("color", COLOR_RECORDING);
           isRecording = true;
@@ -137,6 +167,7 @@
         console.error(error);
         // ユーザーにエラーを通知するUIをここに追加可能
         alert(error.message); // 例: アラートで通知
+        init();
       }
     }
 
