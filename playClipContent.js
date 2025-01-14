3158e1d7faad2c896de121a349190381c9a8fcfd
sabamiso <yabuki1374@gmail.com>
2025-01-14 23:57:57 +0900

    サイトからcookieを経由し値をとることに成功 cookie用にmanifest.jsonに記述を追加

--- playClipContent.js
+++ playClipContent.js
@@ -5,8 +5,33 @@ window.addEventListener("clipListElementsRendered", () => {
 window.addEventListener("clipSelected", () => {
     console.log("このclipを選択しました！");
     //cookieを読み込む
+    const cookies = getCookies();
+    // 取得したCookieをコンソールに表示
+    console.log("Cookies on video:", cookies);
+    //データを変数に格納
     
     //再生機能を起動
 
 });
+
+
+// Cookieを取得する関数
+function getCookies() {
+  const cookies = document.cookie.split("; ");
+  const cookieObj = {};
+  
+  try {
+    // Cookieをオブジェクト形式に変換
+    cookies.forEach(cookie => {
+      const [key, value] = cookie.split("=");
+      cookieObj[key] = decodeURIComponent(value || "");
+    });
+  } catch (error) {
+    console.error("Error parsing cookies:", error);
+  }
+
+  return cookieObj;
+}
+
+
 
